305e874039ae17f8fe1dd490d3caed0ac2fab94a
BHznJNs <441768875@qq.com>
2023-04-28 20:57:49 +0800

    string basic operating updated

--- src/computer/resolvers/operate.rs
+++ src/computer/resolvers/operate.rs
@@ -1,6 +1,6 @@
 use crate::public::value::number::Number;
 use crate::public::value::symbols::Symbols;
-use crate::public::value::value::Value;
+use crate::public::value::value::{Value, Overload};
 
 pub fn operate(
     val1: Value,
@@ -8,8 +8,10 @@ pub fn operate(
     operator: Symbols
 )-> Result<Value, ()> {
     let result =
-    if let (Value::Number(num1), Value::Number(num2)) = (val1, val2) {
+    if let (Value::Number(num1_ref), Value::Number(num2_ref)) = (&val1, &val2) {
         // operating value must be type of Number.
+        let num1 = *num1_ref;
+        let num2 = *num2_ref;
         match operator {
             Symbols::Plus     => num1 + num2,
             Symbols::Minus    => num1 - num2,
@@ -25,6 +27,23 @@ pub fn operate(
                 println!("Unexpected symbol: '{}' at function 'operate'.", operator);
                 return Err(())
             },
+        }
+    } else
+    if let (Value::String(str1_ref), Value::String(str2_ref)) = (&val1, &val2) {
+        let str1 = str1_ref.as_ref().borrow_mut();
+        let str2 = str2_ref.as_ref().borrow();
+        match operator {
+            Symbols::Plus => {
+                let mut cloned = str1.clone();
+                cloned.push_str(&str2);
+                return Ok(Value::create(cloned))
+            },
+            Symbols::CompareEqual =>
+                Number::Int(str1.eq(&*str2) as i64),
+            _ => {
+                println!("Invalid string operate.");
+                return Err(())
+            }
         }
     } else {
         println!("Invalid computing expression: Invalid computing token.");
