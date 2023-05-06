3f8cadb67ae3b85d9bb816969dce6fe47aed41ed
BHznJNs <441768875@qq.com>
2023-05-06 23:02:47 +0800

    version code update

--- src/exec/repl.rs
+++ src/exec/repl.rs
@@ -16,7 +16,7 @@ fn import_all(
 
 pub fn repl(scope: &mut Scope) -> ! {
     // print program name and version
-    println!("Calculator.rs v1.7.4");
+    println!("Calculator.rs v1.7.5");
     // import stantard libraries
     if import_all(scope).is_err() {
         println!("Standard module import error.");
