33fed18d863f40b310e509b42424ad5c18d66a2c
BHznJNs <441768875@qq.com>
2023-06-06 15:11:08 +0800

    headfile resolver optimized

--- src/exec/headfile.rs
+++ src/exec/headfile.rs
@@ -8,12 +8,16 @@ pub fn resolve(headfiles: VecDeque<String>, scope: &mut Scope) {
     let mut headfile_scope = Scope::new(&scope);
 
     for path in headfiles {
-        script::run(path, &mut headfile_scope);
-        let headfile_vars = headfile_scope.global.variables;
+        match script::run(path, &mut headfile_scope) {
+            Ok(_) => {
+                let headfile_vars = headfile_scope.global.variables;
 
-        // init headfile_scope.global.variables
-        headfile_scope.global.variables = build_in::constants();
-        // insert variables into global scope
-        scope.global.variables.extend(headfile_vars);
+                // init headfile_scope.global.variables
+                headfile_scope.global.variables = build_in::constants();
+                // insert variables into global scope
+                scope.global.variables.extend(headfile_vars);
+            },
+            Err(_) => break,
+        }
     }
 }
